8aa2dfb2a35dfc101d37f94528751755f26fd310
SithariGroups <sitharigroups@gmail.com>
2025-01-13 15:44:14 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventController = require('../controllers/eventController');
-
-// Route for adding an event (with image/video upload)
-router.post('/add', eventController.upload.single('eventImage'), eventController.addEvent);
-
-module.exports = router;
--- backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/contactRoutes.js
-const express = require('express');
-const { createContact } = require('../controllers/contactController');  // Correct path
-
-const router = express.Router();
-
-router.post('/', createContact);  // Ensure POST method is configured
-
-module.exports = router;
